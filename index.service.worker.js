88a65a285c6c088bc35d4710961dfd0503b14678
DezNez <dizzon500@inbox.ru>
2026-07-20 04:22:42 +0700

    1.2.4 PWA test release

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784495823|6769989869';
+const CACHE_VERSION = '1784496133|7079498625';
 /** @type {string} */
 const CACHE_PREFIX = 'Shariki MAIN-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
